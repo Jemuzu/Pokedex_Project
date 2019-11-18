42f8dd17d97277d45db53c0e2a4cdea54504ba99
Jemuzu <Jamesiswhoa@gmail.com>
2019-11-18 16:44:04 -0500

    adjusted hrefs, anchors and floated button

--- dex.js
+++ dex.js
@@ -3,8 +3,6 @@
 //A function that belongs to an object.
 //Use the same name as the existing method to overwrite a method.
 
- class Animal {
-   constructor(name){
-     this.name = name
-   }
- }
+alert("Hi, welcome to the world of Pokemon!");
+
+alert("Humans and Pokemon live together in harmony!")
