28e832318fdb67f885c92038e31e738944dd8bbf
Jemuzu <Jamesiswhoa@gmail.com>
2019-11-18 14:23:08 -0500

    Styled buttons, adjusted font through @font-face

---
+++ dex.js
@@ -0,0 +1,10 @@
+
+
+//A function that belongs to an object.
+//Use the same name as the existing method to overwrite a method.
+
+ class Animal {
+   constructor(name){
+     this.name = name
+   }
+ }
